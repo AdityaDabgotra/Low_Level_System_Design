c945d21a5d4c1078a979a1489dce4fbb6cccba14
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-07-18 09:42:58 +0530

    feat: implement OrderManager class for managing food delivery orders

---
+++ Real_Life_Prototype/Tomoto_Food_Delivery/Manager/Order_Manager.h
@@ -0,0 +1,40 @@
+#ifndef ORDER_MANAGER_H
+#define ORDER_MANAGER_H
+
+#include<vector>
+#include<iostream>
+#include"../Models/Order.h"
+using namespace std;
+
+class OrderManager {
+private:
+    vector<Order*> orders;
+    static OrderManager* instance;
+
+    OrderManager() {
+        // private constructor to prevent instantiation
+    }
+
+public:
+    static OrderManager* getInstance() {
+        if (!instance) {
+            instance = new OrderManager();
+        }
+        return instance;
+    }
+
+    void addOrder(Order* order) {
+        orders.push_back(order);
+    }
+
+    void listOrders() {
+        cout<<"\nList of Orders:\n";
+        for (auto order : orders) {
+            cout<<order->getType()<<" order for "<<order->getUser()->getName()<<" | Total: ₹"<<order->getTotal()<<" | At: "<<order->getScheduled()<<endl;
+        }
+    }
+};
+
+OrderManager* OrderManager::instance = nullptr;
+
+endif // ORDER_MANAGER_H
